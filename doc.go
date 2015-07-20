3552ee5b929ec9b250fad48aed00145cb225843e
Mawueli Kofi Adzoe <wallclockbuilder@googlemail.com>
2015-07-20 09:46:02 +0000

    fix #1271 add beego explanation to godoc

--- doc.go
+++ doc.go
@@ -1,6 +1,8 @@
 /*
-beego is an open-source, high-performance, modular,
-full-stack web framework
+beego: an open-source, high-performance, modular, full-stack web framework
+
+beego is used for rapid development of RESTful APIs, web apps and backend services in Go.
+It is inspired by Tornado, Sinatra and Flask. beego has some Go-specific features such as interfaces and struct embedding.
 
 	package main
 
